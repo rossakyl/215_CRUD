1873c285429729da303dd293dd44ca07b0430922
rossakyl <rossakayla01@gmail.com>
2026-07-15 14:46:40 +0700

    feat: complete PUT endpoint

--- index.js
+++ index.js
@@ -63,9 +63,22 @@ app.put('/biodata/:id', async (req, res) => {
             'UPDATE biodata SET nama=$1, nim=$2, kelas=$3 WHERE id=$4 RETURNING *',
             [nama, nim, kelas, id]
         );
-        
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                error: "Data not found"
+            });
+        }
+
+        res.status(200).json({
+            message: 'Data updated successfully',
+            data: result.rows[0],
+        });
     } catch (error) {
-        
+        console.error(error.message);
+        res.status(500).json({
+            error: "Terjadi kesalahan pada server atau database"
+        });
     }
 });
 
