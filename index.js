aba719b3e599fe137f48c1a92a1954783b4465ea
rossakyl <rossakayla01@gmail.com>
2026-07-15 14:48:19 +0700

    feat: complete DELETE endpoint

--- index.js
+++ index.js
@@ -89,8 +89,21 @@ app.delete('/biodata/:id', async (req, res) => {
     try {
         const result = await pool.query('DELETE FROM biodata WHERE id=$1 RETURNING *', [id]);
 
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                error: "Data not found"
+            });
+        }
+
+        res.status(200).json({
+            message: 'Data deleted successfully',
+            data: result.rows[0],
+        });
     } catch (error) {
-    
+        console.error(error.message);
+        res.status(500).json({
+            error: "Terjadi kesalahan pada server atau database"
+        });
     }
 });
 
